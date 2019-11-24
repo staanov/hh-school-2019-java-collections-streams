0b8ed00a71cbc42f7d4aa1721247d3438d7856d7
Stanislav Novikov <stanislav.a.novikov@gmail.com>
2019-11-24 20:25:17 +0300

    Task-1 was done to success output

--- src/tasks/Task1.java
+++ src/tasks/Task1.java
@@ -5,6 +5,7 @@ import common.PersonService;
 import common.Task;
 
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -20,12 +21,9 @@ public class Task1 implements Task {
   // !!! Редактируйте этот метод !!!
   private List<Person> findOrderedPersons(List<Integer> personIds) {
     Set<Person> persons = PersonService.findPersons(personIds);
-    /* Алгоритм:
-    * 1. Из Set получить List
-    * 2. Отсортировать List по personIds
-    * 3. Вернуть отсортированный List
-    * */
-    return Collections.emptyList();
+    return persons.stream()
+            .sorted(Comparator.comparing(Person::getId))
+            .collect(Collectors.toList());
   }
 
   @Override

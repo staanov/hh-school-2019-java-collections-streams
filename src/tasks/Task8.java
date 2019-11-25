08cb58d569aa45fbaa846e63e595db98fa7d7572
Stanislav Novikov <stanislav.a.novikov@gmail.com>
2019-11-25 23:02:07 +0300

    Task-8: refactoring of getDifferentNames(), convertPersonToString(), hasSamePersons() methods was done

--- src/tasks/Task8.java
+++ src/tasks/Task8.java
@@ -3,12 +3,7 @@ package tasks;
 import common.Person;
 import common.Task;
 
-import java.util.Collection;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -23,75 +18,90 @@ P.P.S Здесь ваши правки желательно прокоммент
  */
 public class Task8 implements Task {
 
-  private long count;
+    private long count;
 
-  //Не хотим выдывать апи нашу фальшивую персону, поэтому конвертим начиная со второй
-  public List<String> getNames(List<Person> persons) {
-    if (persons.size() == 0) {
-      return Collections.emptyList();
-    }
-    persons.remove(0);
-    return persons.stream().map(Person::getFirstName).collect(Collectors.toList());
-  }
-
-  //ну и различные имена тоже хочется
-  public Set<String> getDifferentNames(List<Person> persons) {
-    return getNames(persons).stream().distinct().collect(Collectors.toSet());
-  }
-
-  //Для фронтов выдадим полное имя, а то сами не могут
-  public String convertPersonToString(Person person) {
-    String result = "";
-    if (person.getSecondName() != null) {
-      result += person.getSecondName();
-    }
-
-    if (person.getFirstName() != null) {
-      result += " " + person.getFirstName();
-    }
-
-    if (person.getSecondName() != null) {
-      result += " " + person.getSecondName();
-    }
-    return result;
-  }
-
-  // словарь id персоны -> ее имя
-  public Map<Integer, String> getPersonNames(Collection<Person> persons) {
-    Map<Integer, String> map = new HashMap<>(1);
-    for (Person person : persons) {
-      if (!map.containsKey(person.getId())) {
-        map.put(person.getId(), convertPersonToString(person));
-      }
-    }
-    return map;
-  }
-
-  // есть ли совпадающие в двух коллекциях персоны?
-  public boolean hasSamePersons(Collection<Person> persons1, Collection<Person> persons2) {
-    boolean has = false;
-    for (Person person1 : persons1) {
-      for (Person person2 : persons2) {
-        if (person1.equals(person2)) {
-          has = true;
+    //Не хотим выдывать апи нашу фальшивую персону, поэтому конвертим начиная со второй
+    public List<String> getNames(List<Person> persons) {
+        if (persons.size() == 0) {
+            return Collections.emptyList();
         }
-      }
+        persons.remove(0);
+        return persons.stream().map(Person::getFirstName).collect(Collectors.toList());
     }
-    return has;
-  }
 
-  //Выглядит вроде неплохо...
-  public long countEven(Stream<Integer> numbers) {
-    count = 0;
-    numbers.filter(num -> num % 2 == 0).forEach(num -> count++);
-    return count;
-  }
+    //ну и различные имена тоже хочется
+    public Set<String> getDifferentNames(List<Person> persons) {
+        /*
+        staanov: Был возврат Set с помощью Stream API: getNames(persons).stream().distinct().collect(Collectors.toSet())
+        distinct лишний, поскольку Set по умолчанию и так делает коллекцию из неповторяющихся объектов
+        убрав distinct получим: getNames(persons).stream().collect(Collectors.toSet())
+        поскольку нет промежуточных операций, то можно избавиться и от стрима
+        */
+        return new HashSet<>(getNames(persons));
+    }
 
-  @Override
-  public boolean check() {
-    System.out.println("Слабо дойти до сюда и исправить Fail этой таски?");
-    boolean codeSmellsGood = false;
-    boolean reviewerDrunk = false;
-    return codeSmellsGood || reviewerDrunk;
-  }
+    //Для фронтов выдадим полное имя, а то сами не могут
+    public String convertPersonToString(Person person) {
+        String result = "";
+        result += nameNullChecking(person.getSecondName());
+        result += nameNullChecking(person.getFirstName());
+        result += nameNullChecking(person.getMiddleName());
+        return result;
+    }
+
+    /*
+    staanov: Метод проверки любого имени (first, second, middle) на null и возврата имени (если оно не null)
+    во избежание дублирования кода в методе convertPersonToString
+    private поскольку им должен пользоваться только метод convertToString, извне вызов метода nameNullChecking не нужен
+    */
+    private String nameNullChecking(String name) {
+        if (name != null) {
+            return " " + name;
+        } else {
+            return "";
+        }
+    }
+
+    // словарь id персоны -> ее имя
+    public Map<Integer, String> getPersonNames(Collection<Person> persons) {
+        Map<Integer, String> map = new HashMap<>(1);
+        for (Person person : persons) {
+            if (!map.containsKey(person.getId())) {
+                map.put(person.getId(), convertPersonToString(person));
+            }
+        }
+        return map;
+    }
+
+    // есть ли совпадающие в двух коллекциях персоны?
+    public boolean hasSamePersons(Collection<Person> persons1, Collection<Person> persons2) {
+        boolean has = false;
+        /*
+        staanov: Было 2 вложенных цикла for
+        стал 1 цикл for и метод contains,
+        по условию при первом нахождении совпадения цикл завершает свою работы.
+        */
+        for (Person person1 : persons1) {
+            if (persons2.contains(person1)) {
+                has = true;
+                break;
+            }
+        }
+        return has;
+    }
+
+    //Выглядит вроде неплохо...
+    public long countEven(Stream<Integer> numbers) {
+        count = 0;
+        numbers.filter(num -> num % 2 == 0).forEach(num -> count++);
+        return count;
+    }
+
+    @Override
+    public boolean check() {
+        System.out.println("Слабо дойти до сюда и исправить Fail этой таски?");
+        boolean codeSmellsGood = false;
+        boolean reviewerDrunk = false;
+        return codeSmellsGood || reviewerDrunk;
+    }
 }

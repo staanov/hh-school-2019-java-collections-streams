56b658690c1ef20d15dd1e4e2a5d041846939d0c
Stanislav Novikov <stanislav.a.novikov@gmail.com>
2019-11-30 12:55:35 +0300

    Task-1: new realization using extra HashMap and without streams

--- src/tasks/Task1.java
+++ src/tasks/Task1.java
@@ -4,11 +4,9 @@ import common.Person;
 import common.PersonService;
 import common.Task;
 
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /*
 Задача 1
@@ -20,20 +18,27 @@ public class Task1 implements Task {
 
     // !!! Редактируйте этот метод !!!
     /* Оценка сложности алгоритма:
-     * Время: O(n * log n) - из-за алгоритма, где n - количество сортируемых элементов
-     * (как я понял, в Stream.sorted() используется TimSort, а для него худший случай - O(n * log n))
-     * Память: O(n) - из-за создания и возврата списка, где n - количество элементов возвращаемого списка
+     * Время: O(n + n) = O(2n) = O(n) - два НЕ вложенных цикла for
+     * Память: O(n + n) = O(2n) - за счёт дополнительных HashMap и ArrayList
+     * Причемание:
+     * HashMap нужен для того, чтобы можно было по id извлекать элементы Person из коллекции
      * */
     private List<Person> findOrderedPersons(List<Integer> personIds) {
         Set<Person> persons = PersonService.findPersons(personIds);
-        return persons.stream()
-                .sorted(Comparator.comparing(person -> (personIds.get(person.getId() - 1))))
-                .collect(Collectors.toList());
+        Map<Integer, Person> personMap = new HashMap<>();
+        List<Person> sortedPersons = new ArrayList<>();
+        for (Person person : persons) {
+            personMap.put(person.getId(), person);
+        }
+        for (Integer id : personIds) {
+            sortedPersons.add(personMap.get(id));
+        }
+        return sortedPersons;
     }
 
     @Override
     public boolean check() {
-        List<Integer> ids = List.of(1, 2, 3);
+        List<Integer> ids = List.of(3, 1, 2);
 
         return findOrderedPersons(ids).stream()
                 .map(Person::getId)
